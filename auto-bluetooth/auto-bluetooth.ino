4e102073774822a536a2ee26e8a3f6ea84fd1b71
Ema Blanco <emablanco@disroot.com>
2023-06-03 19:02:08 -0300

    auto bluetooth

--- auto-bluetooth/auto-bluetooth.ino
+++ auto-bluetooth/auto-bluetooth.ino
@@ -8,12 +8,12 @@ AF_DCMotor motor4(4);
 //Serial del BT
 SoftwareSerial BTSerial(13,2);
 
+//velocidad de las ruedas
 int velocidad_ruedas = 200;
 
-
-bool encendido3 = false;
-bool encendido4 = false;
-
+//valores para cambiar la marcha
+bool mover3 = false;
+bool mover4 = false;
 
 void setup() {
 
@@ -21,6 +21,7 @@ void setup() {
  Serial.begin(9600);
  BTSerial.begin(9600);
 
+//configurar la velocidad
   motor3.setSpeed(velocidad_ruedas);
   motor4.setSpeed(velocidad_ruedas);
 
@@ -29,31 +30,138 @@ void setup() {
 }
 
 void loop() {
-  
+
+  //sino hay datos en el BTSerial no continua el codigo
   while(BTSerial.available()==0){}
 
-    String datos = BTSerial.readString();
-    Serial.println(datos +" cargados");
-
-    if(datos.equalsIgnoreCase("motor3")){
-      encendido3 = !encendido3;
-      if(encendido3){
-        motor3.run(FORWARD);
-      }
-      else{
-        motor3.run(RELEASE);
-      }
-    }
-
-    if(datos.equalsIgnoreCase("motor4")){
-      encendido4 = !encendido4;
-      if(encendido4){
-        motor4.run(FORWARD);
-      }
-      else{
-        motor4.run(RELEASE);
-      }
-    }
-
+  String datos = BTSerial.readString();
   
+  Serial.println("string recibido: "+datos);
+  
+  setMovimiento(datos);
+  
+}
+
+void setMovimiento(String datos){
+
+  Serial.println("Seteando movimiento");
+  
+  if(datos.equalsIgnoreCase("adelante"))
+    moverAdelante();
+  
+  if(datos.equalsIgnoreCase("atras"))
+    moverAtras();
+  
+  if(datos.equalsIgnoreCase("derecha"))
+    moverDerecha();
+  
+  if(datos.equalsIgnoreCase("izquierda"))
+    moverIzquierda();
+
+  if(datos.equalsIgnoreCase("detener"))
+    detener();
+}
+
+void detener(){
+  Serial.println("DETENER RUEDAS");
+  motor3.run(RELEASE);
+  motor4.run(RELEASE);
+}
+void moverAdelante(){
+
+  Serial.println("Moviendo adelante");
+  if(!mover3){
+    mover3 = !mover3;
+    motor3.run(RELEASE);
+  }
+  if(!mover4){
+    mover4 = !mover4;
+    motor4.run("RELEASE");
+  }
+
+  delay(250);
+  
+  motor3.run(FORWARD);
+  motor4.run(FORWARD);
+}
+  
+void moverAtras(){
+  
+  Serial.println("Moviendo atras");
+  
+  if(mover3){
+    mover3 = !mover3;
+    motor3.run(RELEASE);
+  }
+  
+  if(mover4){
+    mover4 = !mover4;
+    motor4.run("RELEASE");
+  } 
+
+  delay(250);
+  
+  motor3.run(BACKWARD);
+  motor4.run(BACKWARD);
+}
+
+void moverIzquierda(){
+  
+  Serial.println("Moviendo izquierda");
+  
+  if(mover3){
+    mover3 = !mover3;
+    motor3.run(RELEASE);
+  }
+
+  if(!mover4){
+    mover4 = !mover4;
+    motor4.run("RELEASE");
+  } 
+
+  delay(250);
+  
+  motor3.run(BACKWARD);
+  motor4.run(FORWARD);
+
+  delay(3000);
+
+  motor3.run(RELEASE);
+  motor4.run(RELEASE);
+  
+  delay(250);
+  
+  motor3.run(FORWARD);
+  motor4.run(FORWARD);
+
+}
+
+void moverDerecha(){
+  
+  Serial.println("Moviendo derecha");
+  
+  if(!mover3){
+    mover3 = !mover3;
+    motor3.run(RELEASE);
+  }
+
+  if(mover4){
+    mover4 = !mover4;
+    motor4.run("RELEASE");
+  } 
+
+  delay(250);
+  
+  motor3.run(FORWARD);
+  motor4.run(BACKWARD);
+
+  delay(3000);
+
+  motor3.run(RELEASE);
+  motor4.run(RELEASE);
+  
+  delay(250);
+  
+  motor3.run(FORWARD);
+  motor4.run(FORWARD);
 }
